9e4f267fb455857ed35faef6f61b8e95c3cd9593
Olivier CANO <ocano@scaleway.com>
2021-02-25 15:16:16 +0100

    feat: build script

--- scripts/rendu.sh
+++ scripts/rendu.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-go run . input/c_many_ingredients.in
-
-
 zip -r ./output/rendu-$(date +%H%M).zip $(find . -maxdepth 1 -not -name "." -not -name "input" -not -name ".idea" -not -name ".DS_Store" -not -name "go.mod" -not -name "go.sum" -not -name ".gitignore" -not -name "scripts" -not -name "trash" -not -name ".git" -not -name "output")
